af0829e5a519be5a7796740d5544fcb33020653c
Dani G <galanredondo.daniel@jgvaldemora.org>
2025-03-28 09:18:00 +0100

    Segundo commit: Modificaciones a DG.java

--- DG2.java
+++ DG2.java
@@ -8,6 +8,5 @@ package unidad07_clases_y_herencia;
 public class DG {
     public static void main(String[] args) {
         System.out.println("Hola desde DG.java");
-         System.out.println("Hola desde DG2.java");
     }
 }
