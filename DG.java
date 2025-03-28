5c8ed35dd35a5c3d97dfde5573ebadfd16bd0084
Dani G <galanredondo.daniel@jgvaldemora.org>
2025-03-28 09:15:35 +0100

    Primer commit: Añadiendo clase DG.java

---
+++ DG.java
@@ -0,0 +1,12 @@
+
+package unidad07_clases_y_herencia;
+
+/**
+ *
+ * @author galan
+ */
+public class DG {
+    public static void main(String[] args) {
+        System.out.println("Hola desde DG.java");
+    }
+}
